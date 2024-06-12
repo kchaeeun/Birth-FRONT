c46f27697a9c64f0d2608f5fc6a84d5cb8904e18
kchaeeun <oceanball38@naver.com>
2024-06-12 15:05:00 +0900

    최종 수정

--- app/src/main/java/kr/ac/duksung/birth/BluetoothActivity.java
+++ app/src/main/java/kr/ac/duksung/birth/BluetoothActivity.java
@@ -96,7 +96,7 @@ public class BluetoothActivity extends AppCompatActivity
     private AppCompatButton btnCerti;
 
 
-    private static final String BASE_URL = "http://192.168.191.141:8080";
+    private static final String BASE_URL = "http://192.168.0.21:8080";
     private static Retrofit retrofit;
 
     public static Retrofit getRetrofitInstance() {
